2bd79a5ddba84f8962d29d176cebd27c891e2aed
nicodiansk <nicholasprevitali96@gmail.com>
2026-02-24 12:20:12 +0100

    Configure Vitest with jsdom and React support

---
+++ tests/setup.js
@@ -0,0 +1,4 @@
+// ABOUTME: Test setup file that configures the testing environment.
+// ABOUTME: Extends Vitest with DOM-specific matchers from testing-library.
+
+import '@testing-library/jest-dom'
