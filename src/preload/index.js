7413d32d6c81ca346f488b40c3cf66997fade8bd
nicodiansk <nicholasprevitali96@gmail.com>
2026-03-04 09:59:59 +0100

    Expose dir tree, project, and multi-file APIs in preload

--- src/preload/index.js
+++ src/preload/index.js
@@ -4,6 +4,7 @@
 import { contextBridge, ipcRenderer } from 'electron'
 
 contextBridge.exposeInMainWorld('api', {
+  // Single-file operations
   getFilePath: () => ipcRenderer.invoke('get-file-path'),
   readFile: () => ipcRenderer.invoke('read-file'),
   writeFile: (content) => ipcRenderer.invoke('write-file', content),
@@ -12,5 +13,16 @@ contextBridge.exposeInMainWorld('api', {
     const handler = (_event, content) => callback(content)
     ipcRenderer.on('file-changed', handler)
     return () => ipcRenderer.removeListener('file-changed', handler)
-  }
+  },
+
+  // Multi-file tab operations
+  readFileAt: (filePath) => ipcRenderer.invoke('read-file-at', filePath),
+  writeFileAt: (filePath, content) => ipcRenderer.invoke('write-file-at', filePath, content),
+
+  // Project and directory tree
+  getProjectInfo: () => ipcRenderer.invoke('get-project-info'),
+  readDirTree: (dirPath) => ipcRenderer.invoke('read-dir-tree', dirPath),
+  getProjects: () => ipcRenderer.invoke('get-projects'),
+  saveProject: (projectPath) => ipcRenderer.invoke('save-project', projectPath),
+  browseForProject: () => ipcRenderer.invoke('browse-for-project'),
 })
